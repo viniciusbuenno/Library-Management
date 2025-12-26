6e9c5d14dd5d6d37045b8c83992f981246e2b4a9
viniciusbuenno <viniciusbueno123456@gmail.com>
2025-12-25 23:26:35 -0300

    chore: configure IntelliJ IDEA project
    - Add initial source files (Library.java, Loan.java, Main.java)
    - Include IDE configuration files (.idea, vcs.xml, misc.xml, encodings.xml, compiler.xml)
    - Add .gitignore for IntelliJ-specific files

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -1,17 +1,12 @@
 import java.time.LocalDate;
-import java.util.Objects;
 
-
+@lombok.Data
+@lombok.EqualsAndHashCode
 public class Loan implements Comparable<Loan>{
     private final Book book;
     private final User user;
     private final LocalDate loanDate;
     private LocalDate returnDate;
-
-    public Book getBook(){ return this.book; }
-    public User getUser(){ return this.user; }
-    public LocalDate getLoanDate(){ return this.loanDate; }
-    public LocalDate getReturnDate(){ return this.returnDate; }
 
     public void setReturnDate(LocalDate returnDate){
         if(returnDate == null || returnDate.isBefore(this.loanDate)) throw new IllegalArgumentException("\nLoan return date cannot be eager than the loan date or empty");
@@ -35,26 +30,6 @@ public class Loan implements Comparable<Loan>{
         this.user = user;
         this.loanDate = loanDate;
         this.returnDate = loanDate.plusDays(7l);
-    }
-
-    @Override
-    public String toString(){
-        return this.book.toString() + this.user.toString() + "\nLoan Date: " + this.loanDate + "\nReturn Date: " + this.returnDate;
-    }
-
-    @Override
-    public boolean equals(Object other){
-        if (this == other) return true;
-        if (other == null || other.getClass() != this.getClass()) return false;
-        
-        Loan otherLoan = (Loan) other;
-
-        return this.book.equals(otherLoan.getBook()) && this.user.equals(otherLoan.getUser()) && this.loanDate.isEqual(otherLoan.getLoanDate()) && this.returnDate.isEqual(otherLoan.getReturnDate());
-    }
-
-    @Override
-    public int hashCode(){
-        return Objects.hash(this.book, this.user, this.loanDate, this.returnDate);
     }
 
     @Override
